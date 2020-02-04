1b184e9454debf428b432afcf7b96accac79cb23
Cindy Burton <cindy.brton@gmail.com>
2020-02-04 09:18:29 -0700

    getting started

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUE ("Caramelized Onion and Cheddar");
+INSERT INTO burgers (name) VALUE ("Mushroom Swiss");
+INSERT INTO burgers (name) VALUE ("Impossible Burger");
+INSERT INTO burgers (name) VALUE ("Krabby Patty");
+INSERT INTO burgers (name) VALUE ("Royal with Cheese");
+INSERT INTO burgers (name) VALUE ("Big Mac");
+INSERT INTO burgers (name) VALUE ("Bob's Burger");

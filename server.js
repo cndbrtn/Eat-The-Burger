d076317a22fccfdb7acd05be410d48dd25bf8c7a
Cindy Burton <cindy.brton@gmail.com>
2020-02-12 11:43:29 -0700

    heroku still sucks

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,10 +15,9 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 app.use(routes);
 
 app.listen(PORT, () => {
-    console.log("server listening: http://localhost:" + PORT);
+    console.log("server listening:", PORT);
 });
 
